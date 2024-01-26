228c52fe6673ef0cdcec9b5922c4159addedc990
yumcoder <yumcoder.platform@gmail.com>
2024-01-25 23:41:15 -0500

    update readme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,6 @@ import { AppProps } from 'next/app';
 // This default export is required in a new `pages/_app.tsx` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
-            <Component {...pageProps} />
+        <Component {...pageProps} />
     );
 }

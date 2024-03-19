bb64196fcf8622ecb91615a4411321337b1fcaa0
aryan <aryan.sadeghi225@gmail.com>
2024-03-19 16:52:38 +0330

    home page

--- components/home/Contact.tsx
+++ components/home/Contact.tsx
@@ -24,7 +24,7 @@ export default function Contact() {
     const [agreed, setAgreed] = useState(false)
 
     return (
-        <div style={{ background: "linear-gradient(143.6deg, rgba(192, 132, 252, 0) 20.79%, rgba(232, 121, 249, 0.26) 40.92%, rgba(204, 171, 238, 0) 70.35%)" }} className="isolate  px-6 py-24 sm:py-32 lg:px-8">
+        <div style={{ background: "linear-gradient(143.6deg, rgba(192, 132, 252, 0) 20.79%, rgb(121 238 249 / 26%) 40.92%, rgba(204, 171, 238, 0) 70.35%)" }} className="isolate  px-6 py-24 sm:py-32 lg:px-8">
             <div
                 className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
                 aria-hidden="true"
@@ -73,20 +73,7 @@ export default function Contact() {
                             />
                         </div>
                     </div>
-                    <div className="sm:col-span-2">
-                        <label htmlFor="company" className="block text-sm font-semibold leading-6 text-gray-900">
-                            Company
-                        </label>
-                        <div className="mt-2.5">
-                            <input
-                                type="text"
-                                name="company"
-                                id="company"
-                                autoComplete="organization"
-                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            />
-                        </div>
-                    </div>
+
                     <div className="sm:col-span-2">
                         <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
                             Email
@@ -147,39 +134,12 @@ export default function Contact() {
               />
                         </div>
                     </div>
-                    <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
-                        <div className="flex h-6 items-center">
-                            <Switch
-                                checked={agreed}
-                                onChange={setAgreed}
-                                className={classNames(
-                                    agreed ? 'bg-indigo-600' : 'bg-gray-200',
-                                    'flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-                                )}
-                            >
-                                <span className="sr-only">Agree to policies</span>
-                                <span
-                                    aria-hidden="true"
-                                    className={classNames(
-                                        agreed ? 'translate-x-3.5' : 'translate-x-0',
-                                        'h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out'
-                                    )}
-                                />
-                            </Switch>
-                        </div>
-                        <Switch.Label className="text-sm leading-6 text-gray-600">
-                            By selecting this, you agree to our{' '}
-                            <a href="#" className="font-semibold text-indigo-600">
-                                privacy&nbsp;policy
-                            </a>
-                            .
-                        </Switch.Label>
-                    </Switch.Group>
+
                 </div>
                 <div className="mt-10">
                     <button
                         type="submit"
-                        className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        className=" bg-primary block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
                         Let's talk
                     </button>

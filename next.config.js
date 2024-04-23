22e0c2676089bae56aa5ec7f731f8b474c163afe
yumcoder <yumcoder.platform@gmail.com>
2024-04-23 18:33:22 +0000

    update nextjs

--- next.config.js
+++ next.config.js
@@ -9,5 +9,8 @@ const withNextra = nextra({
 
 export default {
   ...withNextra({ reactStrictMode: true }),
-  output: "export",
+  // output: "export",
+  // images: {
+  //   unoptimized: true,
+  // },
 };

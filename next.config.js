c08090a77a9688c4803fb9b4b988c7046da51d5f
yumcoder <yumcoder.platform@gmail.com>
2024-04-23 17:53:50 +0000

    update nextjs cfg

--- next.config.js
+++ next.config.js
@@ -1,18 +1,32 @@
-import nextra from 'nextra'
+// import nextra from 'nextra'
 
-const withNextra = nextra({
+// const withNextra = nextra({
+//   theme: 'nextra-theme-docs',
+//   themeConfig: './theme.config.tsx',
+//   latex: true,
+//   defaultShowCopyCode: true,
+// })
+
+// export default withNextra(
+//    {
+//      reactStrictMode: true,
+//      output: "export",
+//     images: {
+//       unoptimized: true,
+//     },
+//    }
+// )
+
+const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
   latex: true,
   defaultShowCopyCode: true,
 })
 
-export default withNextra(
-   {
-     reactStrictMode: true,
-     output: "export",
-    images: {
-      unoptimized: true,
-    },
-   }
-)
+module.exports = {
+  ...withNextra(),
+  images: {
+    unoptimized: true,
+  },
+};

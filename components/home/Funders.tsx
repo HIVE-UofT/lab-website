def6abc6cea021004f166d7b29c13930139c13a4
ZaRa Shakeri <60937501+Z-Shakeri@users.noreply.github.com>
2026-05-24 02:33:04 +0000

    funders updated

--- components/home/Funders.tsx
+++ components/home/Funders.tsx
@@ -9,6 +9,7 @@ const LOGOS = [
     {image:`/funders/diabetes_canada.png`, size:"120px"},
     {image:`/funders/unicef.png`, size:"150px"},
     {image:`/funders/transform_HF.png`, size:"210px"},
+    {image:`/funders/nfrf.jpg`, size:"150px"},
 ];
 export default () => {
     return (
